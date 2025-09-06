cdacac621f9a893746441a61af31e45ac0a69c42
ADNANE EL MIR <121230114+Sphinex0@users.noreply.github.com>
2025-09-06 10:37:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Set up directories and PATH
 export HOME_DIR=$HOME
-mkdir -p $HOME_DIR/pg $HOME_DIR/pgdata $HOME_DIR/bison $HOME_DIR/m4
+mkdir -p $HOME_DIR/pg $HOME_DIR/pgdata $HOME_DIR/bison $HOME_DIR/m4 $HOME_DIR/flex
 
 # Download and install M4
 cd $HOME_DIR
@@ -22,17 +22,26 @@ cd bison-3.8.2
 make && make install
 export PATH=$HOME_DIR/bison/bin:$PATH
 
+# Download and install Flex
+cd $HOME_DIR
+wget https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
+tar xvzf flex-2.6.4.tar.gz
+cd flex-2.6.4
+./configure --prefix=$HOME_DIR/flex
+make && make install
+export PATH=$HOME_DIR/flex/bin:$PATH
+
 # Download and install PostgreSQL
 cd $HOME_DIR
 wget https://ftp.postgresql.org/pub/source/v17.0/postgresql-17.0.tar.gz
 tar xvzf postgresql-17.0.tar.gz
 cd postgresql-17.0
-./configure --prefix=$HOME_DIR/pg --without-icu
+./configure --prefix=$HOME_DIR/pg --without-icu --without-readline
 make && make install
 export PATH=$HOME_DIR/pg/bin:$PATH
 
 # Add PATH updates to ~/.zshrc for persistence
-echo 'export PATH=$HOME/m4/bin:$HOME/bison/bin:$HOME/pg/bin:$PATH' >> ~/.zshrc
+echo 'export PATH=$HOME/m4/bin:$HOME/bison/bin:$HOME/flex/bin:$HOME/pg/bin:$PATH' >> ~/.zshrc
 
 # Initialize and start PostgreSQL
 initdb -D $HOME_DIR/pgdata
